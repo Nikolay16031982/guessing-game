31cdf4ce642931531faea2b12d1667ac9141d01e
Nikolay <kolya.kuzmichev.82@mail.ru>
2023-02-27 00:37:27 +0300

    feat: end task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
     constructor() {}
-
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.guessing = Math.round((this.max + this.min) / 2);
+      return this.guessing;
     }
-
+  
     lower() {
-
+      this.max = this.guessing;
     }
-
+  
     greater() {
-
+      this.min = this.guessing;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
